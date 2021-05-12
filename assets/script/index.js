e739e6eae84320b58083d44e35951e17c5ee8ff7
haybuka <paschal.nduleo@gmail.com>
2021-05-13 00:42:30 +0100

    added conditional for center sliding

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,61 +2,81 @@
 let harmburger = document.querySelector('.harmburger');
 let navAlt = document.querySelector('.nav-alt');
 let asides = [...document.querySelectorAll('.slide-aside')]
+let centerSlide = [...document.querySelectorAll('.center-slide')]
 
 harmburger.addEventListener('click',()=>{
   navAlt.classList.toggle('slide-left');
   harmburger.classList.toggle('roll')
 })
-$('.center').slick({
-      centerMode: true,
-      centerPadding: '60px',
-      slidesToShow: 3,
-      arrows:false,
-      autoplay:true,
-      responsive: [
-      {
-          breakpoint: 1240,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '20px',
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '40px',
-            slidesToShow: 2,
-            autoplaySpeed:2000
-          }
-        },
-        {
-          breakpoint: 630,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '0px',
-            slidesToShow: 2,
-            autoplaySpeed:1500,
-            cssEase:'ease-out'
-          }
-        },
-        {
-          breakpoint: 630,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '0px',
-            slidesToShow: 1,
-            autoplaySpeed:1500,
-            cssEase:'ease-out'
-          }
+if(centerSlide.length < 2){
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    arrows:false,
+    autoplay:true
+  })
+} else if(centerSlide.length < 3){
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    arrows:false,
+    autoplay:true
+  })
+} else{
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    arrows:false,
+    autoplay:true,
+    responsive: [
+    {
+        breakpoint: 1240,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 3
         }
-      ]
-    });
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2,
+          autoplaySpeed:2000
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 2,
+          autoplaySpeed:1500,
+          cssEase:'ease-out'
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1,
+          autoplaySpeed:1500,
+          cssEase:'ease-out'
+        }
+      }
+    ]
+  });
+}
+
 
 if (asides.length < 2) {
    
